31f460976999ca2c98efc6634440d421ecae7dfd
Anvitha G Rao <119688233+anvitha-rao10@users.noreply.github.com>
2025-09-17 05:47:00 +0000

    Updated code 6

--- src/calculations.py
+++ src/calculations.py
@@ -3,17 +3,17 @@ import math
 def area_of_circle(radius):
     if radius < 0:
         raise ValueError("Radius cannot be negative")
-    return math.pi * radius * radius
+    return math.pi * radius ** 2
 
 def get_nth_fibonacci(n):
     if n < 0:
-        raise ValueError("Fibonacci is not defined for negative numbers")
+        raise ValueError("Fibonacci number is not defined for negative integers")
     elif n == 0:
         return 0
     elif n == 1:
         return 1
-    else:
-        a, b = 0, 1
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        return b
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
